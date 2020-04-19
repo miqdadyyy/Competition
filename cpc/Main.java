e14dfe1fe9a040e54c1662e2e591859c8361b846
Miqdad Yanuar Farcha <miqdad.farcha@gmail.com>
2020-04-20 01:06:26 +0700

    WIP

---
+++ cpc/Main.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader reader = new FastReader();
+        int SUM_TEST_CASE = reader.nextInt();
+		long startTime = System.currentTimeMillis();
+        for (int CASE = 1; CASE <= SUM_TEST_CASE; CASE++) {
+            System.out.printf("CASE #%d: ", CASE);
+            solve(reader, CASE);
+        }
+		long endTime = System.currentTimeMillis();
+		time(startTime, endTime);
+    }
+
+    static void solve(FastReader reader, int CASE) {
+        
+    }
+
+	static void time(long start, long finish){
+		double time = (finish - start) / 1000;
+		System.err.printf("Time execution : %.3f", time);
+	}
+}
---
+++ cpc/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        int SUM_TEST_CASE = 100;
+        for (int CASE = 1; CASE <= SUM_TEST_CASE; ++CASE) {
+
+        }
+    }
+}
